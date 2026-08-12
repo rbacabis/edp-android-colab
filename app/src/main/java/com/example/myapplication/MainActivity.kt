64db602953af90c70760887fbdd1c09e0ca1cbc9
Rain Robert Bacabis <rbacabis24725@liceo.edu.ph>
2026-08-12 10:36:31 +0800

    lab-activity-8

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,31 +3,19 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                val navController = rememberNavController()
-                
-                NavHost(navController = navController, startDestination = Home) {
-                    composable<Home> {
-                        HomeScreen(onShowGreeting = { typedName ->
-                            navController.navigate(Greeting(userName = typedName))
-                        })
-                    }
-                    composable<Greeting> { backStackEntry ->
-                        val greeting: Greeting = backStackEntry.toRoute()
-                        GreetingScreen(userName = greeting.userName)
-                    }
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ProfileScreen()
                 }
             }
         }
